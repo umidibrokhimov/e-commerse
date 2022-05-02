8e7b810960c143ae27bee0d2369244b5d6fbf7e9
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-02 18:25:25 +0500

    added products list and products categories for models

--- ecommerse/admin.py
+++ ecommerse/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(HomeSlider)
+admin.site.register(ProductsList)
+admin.site.register(ProductsCategory)
--- ecommerse/models.py
+++ ecommerse/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class ProductsCategory(models.Model):
+    class Meta():
+        verbose_name = 'Product category'
+        verbose_name_plural = 'Products categories'
+    
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
 class HomeSlider(models.Model):
     class Meta():
         verbose_name = 'Slide'
@@ -8,3 +18,16 @@ class HomeSlider(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=50)
     image = models.ImageField()
+
+class ProductsList(models.Model):
+    class Meta():
+        verbose_name = 'Product list'
+        verbose_name_plural = 'Products lists'
+    
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    price = models.IntegerField()
+    category = models.OneToOneField(ProductsCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
